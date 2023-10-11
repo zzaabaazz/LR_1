5bdf2a32ec876a93a5809fe0d33931de8463453a
zzaabaazz <58651910+zzaabaazz@users.noreply.github.com>
2023-10-11 15:43:12 +0300

    EXS 1

--- LR_1/Form1.Designer.cs
+++ LR_1/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace LR_1
+namespace LR_1
 {
     partial class Form1
     {
@@ -31,38 +31,135 @@
             button1 = new Button();
             textBox1 = new TextBox();
             textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            label4 = new Label();
+            textBox4 = new TextBox();
+            button2 = new Button();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(296, 127);
+            button1.Location = new Point(296, 182);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
+            button1.Size = new Size(100, 32);
             button1.TabIndex = 0;
-            button1.Text = "button1";
+            button1.Text = "Расчитать";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(285, 69);
+            textBox1.Location = new Point(296, 76);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
+            textBox1.Size = new Size(111, 23);
             textBox1.TabIndex = 1;
-            textBox1.TextChanged += textBox1_TextChanged;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(285, 98);
+            textBox2.Location = new Point(296, 105);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(100, 23);
+            textBox2.Size = new Size(111, 23);
             textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(296, 47);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(111, 23);
+            textBox3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(229, 50);
+            label1.Name = "label1";
+            label1.Size = new Size(45, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Радиус";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(229, 76);
+            label2.Name = "label2";
+            label2.Size = new Size(47, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Высота";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(210, 108);
+            label3.Name = "label3";
+            label3.Size = new Size(80, 15);
+            label3.TabIndex = 6;
+            label3.Text = "Образующая";
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(255, 145);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(64, 19);
+            checkBox1.TabIndex = 7;
+            checkBox1.Text = "Объем";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(325, 145);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(154, 19);
+            checkBox2.TabIndex = 8;
+            checkBox2.Text = "Площадь Поверхности";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(229, 223);
+            label4.Name = "label4";
+            label4.Size = new Size(38, 15);
+            label4.TabIndex = 10;
+            label4.Text = "Ответ";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(296, 220);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(219, 23);
+            textBox4.TabIndex = 9;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(296, 271);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 11;
+            button2.Text = "ВЫХОД";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(label4);
+            Controls.Add(textBox4);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox3);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
             Controls.Add(button1);
@@ -77,5 +174,14 @@
         private Button button1;
         private TextBox textBox1;
         private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private Label label4;
+        private TextBox textBox4;
+        private Button button2;
     }
 }
