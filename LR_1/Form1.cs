8be923d80ef76f4936305e7e814b9b275a7f282d
zzaabaazz <58651910+zzaabaazz@users.noreply.github.com>
2023-10-11 15:41:24 +0300

    EXS 1(form)

--- LR_1/Form1.cs
+++ LR_1/Form1.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Data;
+
 namespace LR_1
 {
     public partial class Form1 : Form
@@ -6,15 +9,38 @@ namespace LR_1
         {
             InitializeComponent();
         }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
+            double h = Convert.ToSingle(textBox1.Text);
+            double R = Convert.ToSingle(textBox3.Text);
+            double L = Convert.ToSingle(textBox2.Text);
+            const double PI = 3.1415;
 
+
+            double V = 0.3334 * PI * R * R * h;
+            double S = PI * R * (L + R);
+            if (checkBox1.Checked && checkBox2.Checked)
+            {
+                textBox4.Text = V.ToString() + ";" + S.ToString();
+            }
+            else if (checkBox1.Checked)
+            {
+                textBox4.Text = V.ToString();
+
+
+            }
+            else if (checkBox2.Checked)
+            {
+                textBox4.Text = S.ToString();
+            }
+            else
+            {
+                textBox4.Text = "Не выбран режим!";
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
