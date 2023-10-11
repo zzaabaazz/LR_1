60d9522466eaca662183d39009d499b17b31a037
zzaabaazz <58651910+zzaabaazz@users.noreply.github.com>
2023-10-11 17:51:01 +0300

    EX1 p1

--- LR_1/Form1.cs
+++ LR_1/Form1.cs
@@ -9,27 +9,38 @@ namespace LR_1
         {
             InitializeComponent();
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
-            string S = "ФИО:" + textBox3.Text + " " + textBox1.Text + " " + textBox2.Text + "\nУвлечения: ";
+            double h = Convert.ToSingle(textBox1.Text);
+            double R = Convert.ToSingle(textBox3.Text);
+            double L = Convert.ToSingle(textBox2.Text);
+            const double PI = 3.1415;
 
-            var checkboxList_U = new List<CheckBox>
-            {checkBox1,checkBox2,checkBox3,checkBox4,checkBox5,checkBox6};
-            var messages_U = checkboxList_U.Where(x => x.Checked).Select(x => x.Text);
-            string U = string.Join(", ", messages_U);
-            U = U + ("\nОбразование: ");
-            var checkboxList_O = new List<RadioButton>
-            {radioButton1,radioButton2,radioButton3};
-            var messages_O = checkboxList_O.Where(x => x.Checked).Select(x => x.Text);
-            string O = string.Join(", ", messages_O);
-            O = O + ("\nПол: ");
-            var checkboxList_G = new List<RadioButton>
-            {radioButton5,radioButton6};
-            var messages_G = checkboxList_G.Where(x => x.Checked).Select(x => x.Text);
-            string G = string.Join(", ", messages_G);
 
-            MessageBox.Show(S + U +O+G, "A message", MessageBoxButtons.OKCancel);
+            double V = 0.3334 * PI * R * R * h;
+            double S = PI * R * (L + R);
+            if (checkBox1.Checked && checkBox2.Checked)
+            {
+                textBox4.Text = V.ToString() + ";" + S.ToString();
+            }
+            else if (checkBox1.Checked)
+            {
+                textBox4.Text = V.ToString();
+
+
+            }
+            else if (checkBox2.Checked)
+            {
+                textBox4.Text = S.ToString();
+            }
+            else
+            {
+                textBox4.Text = "РќРµ РІС‹Р±СЂР°РЅ СЂРµР¶РёРј!";
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
