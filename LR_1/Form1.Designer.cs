33f6334b6a267a2ab679f2cd892e40fce95e8574
zzaabaazz <58651910+zzaabaazz@users.noreply.github.com>
2023-10-11 17:51:47 +0300

    EX1 p2

--- LR_1/Form1.Designer.cs
+++ LR_1/Form1.Designer.cs
@@ -28,49 +28,47 @@ namespace LR_1
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
             textBox1 = new TextBox();
             textBox2 = new TextBox();
             textBox3 = new TextBox();
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
-            GroupBox1 = new GroupBox();
-            checkBox4 = new CheckBox();
-            checkBox5 = new CheckBox();
-            checkBox6 = new CheckBox();
-            checkBox3 = new CheckBox();
-            checkBox2 = new CheckBox();
             checkBox1 = new CheckBox();
-            groupBox2 = new GroupBox();
-            radioButton3 = new RadioButton();
-            radioButton2 = new RadioButton();
-            radioButton1 = new RadioButton();
-            groupBox3 = new GroupBox();
-            radioButton5 = new RadioButton();
-            radioButton6 = new RadioButton();
-            button1 = new Button();
-            GroupBox1.SuspendLayout();
-            groupBox2.SuspendLayout();
-            groupBox3.SuspendLayout();
+            checkBox2 = new CheckBox();
+            label4 = new Label();
+            textBox4 = new TextBox();
+            button2 = new Button();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(296, 182);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 32);
+            button1.TabIndex = 0;
+            button1.Text = "Расчитать";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(303, 15);
+            textBox1.Location = new Point(296, 76);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(111, 23);
             textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(208, 44);
+            textBox2.Location = new Point(296, 105);
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(111, 23);
             textBox2.TabIndex = 2;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(91, 12);
+            textBox3.Location = new Point(296, 47);
             textBox3.Name = "textBox3";
             textBox3.Size = new Size(111, 23);
             textBox3.TabIndex = 3;
@@ -78,242 +76,112 @@ namespace LR_1
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(24, 15);
+            label1.Location = new Point(229, 50);
             label1.Name = "label1";
-            label1.Size = new Size(58, 15);
+            label1.Size = new Size(45, 15);
             label1.TabIndex = 4;
-            label1.Text = "Фамилия";
+            label1.Text = "Радиус";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(256, 18);
+            label2.Location = new Point(229, 76);
             label2.Name = "label2";
-            label2.Size = new Size(31, 15);
+            label2.Size = new Size(47, 15);
             label2.TabIndex = 5;
-            label2.Text = "Имя";
+            label2.Text = "Высота";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(141, 44);
+            label3.Location = new Point(210, 108);
             label3.Name = "label3";
-            label3.Size = new Size(58, 15);
+            label3.Size = new Size(80, 15);
             label3.TabIndex = 6;
-            label3.Text = "Отчество";
-            // 
-            // GroupBox1
-            // 
-            GroupBox1.Controls.Add(checkBox4);
-            GroupBox1.Controls.Add(checkBox5);
-            GroupBox1.Controls.Add(checkBox6);
-            GroupBox1.Controls.Add(checkBox3);
-            GroupBox1.Controls.Add(checkBox2);
-            GroupBox1.Controls.Add(checkBox1);
-            GroupBox1.Location = new Point(63, 106);
-            GroupBox1.Name = "GroupBox1";
-            GroupBox1.Size = new Size(118, 172);
-            GroupBox1.TabIndex = 9;
-            GroupBox1.TabStop = false;
-            GroupBox1.Text = "Увлечения";
-            // 
-            // checkBox4
-            // 
-            checkBox4.AutoSize = true;
-            checkBox4.Location = new Point(11, 100);
-            checkBox4.Name = "checkBox4";
-            checkBox4.Size = new Size(56, 19);
-            checkBox4.TabIndex = 5;
-            checkBox4.Text = "Театр";
-            checkBox4.UseVisualStyleBackColor = true;
-            // 
-            // checkBox5
-            // 
-            checkBox5.AutoSize = true;
-            checkBox5.Location = new Point(11, 122);
-            checkBox5.Name = "checkBox5";
-            checkBox5.Size = new Size(61, 19);
-            checkBox5.TabIndex = 4;
-            checkBox5.Text = "Танцы";
-            checkBox5.UseVisualStyleBackColor = true;
-            // 
-            // checkBox6
-            // 
-            checkBox6.AutoSize = true;
-            checkBox6.Location = new Point(11, 147);
-            checkBox6.Name = "checkBox6";
-            checkBox6.Size = new Size(84, 19);
-            checkBox6.TabIndex = 3;
-            checkBox6.Text = "Рукоделие";
-            checkBox6.UseVisualStyleBackColor = true;
-            // 
-            // checkBox3
-            // 
-            checkBox3.AutoSize = true;
-            checkBox3.Location = new Point(11, 75);
-            checkBox3.Name = "checkBox3";
-            checkBox3.Size = new Size(54, 19);
-            checkBox3.TabIndex = 2;
-            checkBox3.Text = "Кино";
-            checkBox3.UseVisualStyleBackColor = true;
-            // 
-            // checkBox2
-            // 
-            checkBox2.AutoSize = true;
-            checkBox2.Location = new Point(11, 50);
-            checkBox2.Name = "checkBox2";
-            checkBox2.Size = new Size(69, 19);
-            checkBox2.TabIndex = 1;
-            checkBox2.Text = "Музыка";
-            checkBox2.UseVisualStyleBackColor = true;
+            label3.Text = "Образующая";
             // 
             // checkBox1
             // 
             checkBox1.AutoSize = true;
-            checkBox1.Location = new Point(11, 25);
+            checkBox1.Location = new Point(255, 145);
             checkBox1.Name = "checkBox1";
-            checkBox1.Size = new Size(60, 19);
-            checkBox1.TabIndex = 0;
-            checkBox1.Text = "Спорт";
+            checkBox1.Size = new Size(64, 19);
+            checkBox1.TabIndex = 7;
+            checkBox1.Text = "Объем";
             checkBox1.UseVisualStyleBackColor = true;
             // 
-            // groupBox2
+            // checkBox2
             // 
-            groupBox2.Controls.Add(radioButton3);
-            groupBox2.Controls.Add(radioButton2);
-            groupBox2.Controls.Add(radioButton1);
-            groupBox2.Location = new Point(234, 101);
-            groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(200, 85);
-            groupBox2.TabIndex = 10;
-            groupBox2.TabStop = false;
-            groupBox2.Text = "Образование";
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(325, 145);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(154, 19);
+            checkBox2.TabIndex = 8;
+            checkBox2.Text = "Площадь Поверхности";
+            checkBox2.UseVisualStyleBackColor = true;
             // 
-            // radioButton3
+            // label4
             // 
-            radioButton3.AutoSize = true;
-            radioButton3.Location = new Point(8, 55);
-            radioButton3.Name = "radioButton3";
-            radioButton3.Size = new Size(181, 19);
-            radioButton3.TabIndex = 2;
-            radioButton3.TabStop = true;
-            radioButton3.Text = "Высшее профессиональное";
-            radioButton3.UseVisualStyleBackColor = true;
+            label4.AutoSize = true;
+            label4.Location = new Point(229, 223);
+            label4.Name = "label4";
+            label4.Size = new Size(38, 15);
+            label4.TabIndex = 10;
+            label4.Text = "Ответ";
             // 
-            // radioButton2
+            // textBox4
             // 
-            radioButton2.AutoSize = true;
-            radioButton2.Location = new Point(8, 36);
-            radioButton2.Name = "radioButton2";
-            radioButton2.Size = new Size(182, 19);
-            radioButton2.TabIndex = 1;
-            radioButton2.TabStop = true;
-            radioButton2.Text = "Среднее профессиональное";
-            radioButton2.UseVisualStyleBackColor = true;
+            textBox4.Location = new Point(296, 220);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(219, 23);
+            textBox4.TabIndex = 9;
             // 
-            // radioButton1
+            // button2
             // 
-            radioButton1.AutoSize = true;
-            radioButton1.Location = new Point(8, 17);
-            radioButton1.Name = "radioButton1";
-            radioButton1.Size = new Size(111, 19);
-            radioButton1.TabIndex = 0;
-            radioButton1.TabStop = true;
-            radioButton1.Text = "Среднее общее";
-            radioButton1.UseVisualStyleBackColor = true;
-            // 
-            // groupBox3
-            // 
-            groupBox3.Controls.Add(radioButton5);
-            groupBox3.Controls.Add(radioButton6);
-            groupBox3.Location = new Point(234, 206);
-            groupBox3.Name = "groupBox3";
-            groupBox3.Size = new Size(119, 72);
-            groupBox3.TabIndex = 11;
-            groupBox3.TabStop = false;
-            groupBox3.Text = "Пол";
-            // 
-            // radioButton5
-            // 
-            radioButton5.AutoSize = true;
-            radioButton5.Location = new Point(8, 36);
-            radioButton5.Name = "radioButton5";
-            radioButton5.Size = new Size(75, 19);
-            radioButton5.TabIndex = 1;
-            radioButton5.TabStop = true;
-            radioButton5.Text = "Женский";
-            radioButton5.UseVisualStyleBackColor = true;
-            // 
-            // radioButton6
-            // 
-            radioButton6.AutoSize = true;
-            radioButton6.Location = new Point(8, 17);
-            radioButton6.Name = "radioButton6";
-            radioButton6.Size = new Size(77, 19);
-            radioButton6.TabIndex = 0;
-            radioButton6.TabStop = true;
-            radioButton6.Text = "Мужской";
-            radioButton6.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            button1.Font = new Font("Times New Roman", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
-            button1.Location = new Point(371, 206);
-            button1.Name = "button1";
-            button1.Size = new Size(119, 75);
-            button1.TabIndex = 12;
-            button1.Text = "ОК";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            button2.Location = new Point(296, 271);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 11;
+            button2.Text = "ВЫХОД";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(515, 302);
-            Controls.Add(button1);
-            Controls.Add(groupBox3);
-            Controls.Add(groupBox2);
-            Controls.Add(GroupBox1);
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(label4);
+            Controls.Add(textBox4);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(textBox3);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
+            Controls.Add(button1);
             Name = "Form1";
             Text = "Лабораторная работа №1";
-            GroupBox1.ResumeLayout(false);
-            GroupBox1.PerformLayout();
-            groupBox2.ResumeLayout(false);
-            groupBox2.PerformLayout();
-            groupBox3.ResumeLayout(false);
-            groupBox3.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
         private TextBox textBox1;
         private TextBox textBox2;
         private TextBox textBox3;
         private Label label1;
         private Label label2;
         private Label label3;
-        private GroupBox GroupBox1;
-        private CheckBox checkBox4;
-        private CheckBox checkBox5;
-        private CheckBox checkBox6;
-        private CheckBox checkBox3;
-        private CheckBox checkBox2;
         private CheckBox checkBox1;
-        private GroupBox groupBox2;
-        private RadioButton radioButton3;
-        private RadioButton radioButton2;
-        private RadioButton radioButton1;
-        private GroupBox groupBox3;
-        private RadioButton radioButton5;
-        private RadioButton radioButton6;
-        private Button button1;
+        private CheckBox checkBox2;
+        private Label label4;
+        private TextBox textBox4;
+        private Button button2;
     }
 }
